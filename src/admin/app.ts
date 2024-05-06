0842dbe1acb38b4c4baa37643b8c9d8402001494
qraxiss <qraxiss@gmail.com>
2024-05-06 17:26:02 +0300

    admin-panel: logo added

---
+++ src/admin/app.ts
@@ -0,0 +1,24 @@
+import logo from './extensions/logo.png'
+
+const config = {
+    tutorials: false,
+    locales: ['tr', 'en'],
+    notifications: { releases: false },
+    head: {
+        favicon: logo
+    },
+    auth: {
+        logo: logo
+    },
+    menu: {
+        logo: logo
+    }
+}
+
+export default {
+    config,
+    bootstrap(app: any) {
+        console.log('test', app)
+    },
+    register(app: any) {}
+}
